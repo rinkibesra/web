e4779a7100fb96276ac6b753feed80994c7d84b9
Rinki <rinkibesra.bca.aec@gmail.com>
2026-06-19 09:19:45 +0530

    string

--- jss/script.js
+++ jss/script.js
@@ -58,30 +58,78 @@
 // let l_name="besra";
 // let full_name=f_name +" "+ l_name;
 // console.log(full_name);
-let size=prompt("Enter the size ");
-if(size=="XL"){
-    console.log("Extra Large");
-}
-else if(size=="L"){
-    console.log("PRICE 250");
-}
-else if(size=="M"){
-    console.log("PRICE 200");
-}
-else if(size=="S"){
-    console.log("PRICE 150");
-}
-let string=prompt("Enter the string");
-if((string[0]=='a'||string[0]=='A')&&(string.length>=3)){
-    console.log("string is a good string");
-}
-else{
-    console.log("string is not a good string");
-}
-let num=prompt("Enter the number");
-if(num%10==0){
-    console.log("number is divisible by 10");
+// let size=prompt("Enter the size ");
+// if(size=="XL"){
+//     console.log("Extra Large");
+// }
+// else if(size=="L"){
+//     console.log("PRICE 250");
+// }
+// else if(size=="M"){
+//     console.log("PRICE 200");
+// }
+// else if(size=="S"){
+//     console.log("PRICE 150");
+// }
+// let string=prompt("Enter the string");
+// if((string[0]=='a'||string[0]=='A')&&(string.length>=3)){
+//     console.log("string is a good string");
+// }
+// else{
+//     console.log("string is not a good string");
+// }
+// let num=prompt("Enter the number");
+// if(num%10==0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+// let name=prompt("Enter the name");
+// let age=prompt("Enter the age");
+// console.log(`${name} is ${age} years old`);
+// let quator=prompt("enter quator");
+// switch (quator){
+//     case 'q1':
+//         console.log("jan,feb,mar");
+//         break;
+//     case 'q2': 
+//     console.log("april,may,jun");
+//     break;  
+//     case 'q3':
+//         console.log("july,aug,sep");
+//         break;
+//     case 'q4':
+//         console.log("oct,nov,dec");
+//         break;     
+// } 
+// let string=prompt("enter a string");
+// if((string[0]=='a'||string[0]=='A')&&(string.length>=5)){
+//     console.log("golden string");
+// }else{
+//     console.log("not a golden string");
+// }
+// let a=prompt("enter a num");
+// let b=prompt("enter b num");
+// let c=prompt("enter c num");
+// if(a>b &&a>c){
+//     console.log("a id greatest");
+// }
+// else if(b>a && b>c){
+//     console.log(" b is greatest");
+// }
+// else{
+//     console.log("c is greatest");
+// }
+let a=prompt("enter a num");
+let b=prompt("enter b num");
+if((a%10)==(b%10)){
+    console.log("same");
 }else{
-    console.log("number is not divisible by 10");
+    console.log("not same");
 }
+
+
+
+
+
 
---
+++ jss/string.js
@@ -0,0 +1,20 @@
+let string="     Rinki     ";
+console.log(string.trim());
+console.log(string.toLowerCase());
+console.log(string.toUpperCase());
+console.log(string.indexOf("inki"));
+console.log(string.toLowerCase().indexOf("e"));
+let st=" I Love Coding";
+console.log(st.slice(5));
+console.log(st.slice(7,14));
+console.log(st.slice(-3));
+console.log(st.replace("Love","enjoy"));
+let msg="help!";
+console.log(msg.trim().toUpperCase());
+let name="ApnaCollege";
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
+console.log(name.slice(-7).replace("ll","tt"));
+
+
