2cfbef794086fe16a4129654189fdfbcbc940348
Rinki <rinkibesra.bca.aec@gmail.com>
2026-06-27 09:43:04 +0530

    dom

--- Dom/app.js
+++ Dom/app.js
@@ -2,9 +2,20 @@ let smallImg=document.getElementsByClassName("oldImg");
 for(let i=0;i<=smallImg.length;i++){
     console.dir(smallImg[i]);
 }
-let newImg=document.getElementsByClassName("oldImg");
-for(let i=0;i<=newImg.length;i++){
-    newImg[i].src="spiderman_img.png";
-console.log(`the image on ${i} is changed`);
-}
-let text=document.getElementsByTagName("p")[0].innerText;
+// let newImg=document.getElementsByClassName("oldImg");
+// for(let i=0;i<=newImg.length;i++){
+//     newImg[i].src="spiderman_img.png";
+// console.log(`the image on ${i} is changed`);
+// }
+// let change=document.getElementsByTagName("p")[0].innerText="abc";
+console.dir(document.querySelector("p"));
+console.dir(document.querySelectorAll("P"));
+console.dir(document.querySelectorAll("div a"));
+let para=document.querySelector("p");
+console.dir(para.innerHTML);
+console.dir(para.textContent);
+console.dir(para.innerText);
+// para.innerText="I am Peter Parker";
+para.innerHTML="<b>I am peter parker</b>";
+let heading=document.querySelector("h1");
+heading.innerHTML=`<u> ${heading.innerText} </ul>`
