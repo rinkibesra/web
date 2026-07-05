05d8c1573c3dfb56bc037dfe7d05c9f3f939632f
Rinki <rinkibesra.bca.aec@gmail.com>
2026-07-05 20:35:35 +0530

    dom

--- Dom/app.js
+++ Dom/app.js
@@ -22,8 +22,31 @@ heading.innerHTML=`<u> ${heading.innerText} </ul>`
 //getter setter
 let img=document.querySelector("img");
 img.getAttribute("id");
-img.setAttribute("src","creation_3.jpeg" );
-let class_img=document.querySelector("img");
-console.dir(class_img.classList);
-let class_img=document.querySelector("img");
-console.dir(class_img.classList);
+// img.setAttribute("src","creation_3.jpeg" );
+//classList
+
+console.dir(heading.classList);
+heading.classList.add("green");
+heading.classList.add("underline");
+heading.classList.remove("green");
+heading.classList.toggle("green");
+//Navigation
+let h4=document.querySelector("h4");
+
+console.dir(h4.parentElement);
+console.dir(h4.children);
+let ul=document.querySelector("ul");
+
+console.dir(ul.children[0].nextElementSibling);
+console.dir(ul.children[1].nextElementSibling);
+img.previousElementSibling.style.backgroundColor="pink";
+let p=document.querySelector("p");
+let btn=document.createElement('button');
+btn.innerText="clickme";
+console.dir(p.insertAdjacentElement('beforebegin',btn));
+console.dir(box.insertAdjacentElement('afterend',btn));
+console.dir(box.insertAdjacentElement('beforeend',btn));
+console.dir(box.insertAdjacentElement('beforebegin',btn));
+
+
+
