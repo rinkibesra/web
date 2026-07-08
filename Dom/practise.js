42bdde832c79337639c2c9e3236e02ce005ca7be
Rinki <rinkibesra.bca.aec@gmail.com>
2026-07-08 23:51:49 +0530

    dom events

--- Dom/practise.js
+++ Dom/practise.js
@@ -43,3 +43,57 @@ h1.classList.add("text");
 let para=document.createElement("p");
 para.innerHTML="Apna College <b>Delta</b> Practise";
 document.querySelector("body").appendChild(para);
+
+let p2=document.querySelector("p");
+p2.addEventListener("click",function(){
+    console.log("u clicked the paragraph");});
+    let box=document.querySelector(".box");
+    box.addEventListener("mouseover",function(){
+        console.log("u hovered the box");
+    });
+
+    //this in dom event
+    btn=document.querySelector("button");
+    h1=document.querySelector("h1");
+    h3=document.querySelector("h3");
+  div=document.querySelector(".para");
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor="red";}
+btn.addEventListener("click",changeColor);
+h1.addEventListener("click",changeColor);
+h3.addEventListener("click",changeColor);
+div.addEventListener("click",changeColor);
+//KEYBOARD EVENTS
+let input1=document.querySelector("input");
+input1.addEventListener("keydown",function(e){
+    console.dir(e);
+    console.dir(e.key);
+    console.dir(e.code);
+    console.log(`the key was  pressed`);
+});
+input1.addEventListener("keyup",function(e){
+    console.log(`the key was released`);
+});
+let inp=document.querySelector("#input");
+inp.addEventListener("keydown",function(e){
+    console.log("code=" , e.code);
+    if(e.code=="KeyU"){
+        console.log("character moved up");
+    }else if(e.code=="KeyD"){
+        console.log("character moved down");
+    }else if(e.code=="KeyL"){
+        console.log("character moved left");
+    }  else if(e.code=="KeyR"){
+        console.log("character moved right");
+    }
+});
+let form=document.querySelector("form");
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    console.dir(form);
+    let user=this.elements[0];
+    let password=this.elements[1];
+    alert(`the username is ${user} and the password is ${password}`);
+    
+});
